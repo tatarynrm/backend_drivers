c00a86836b34c050d9f5c1996604420acea6a125
tatarynrm <tatarynrm@gmail.com>
2024-10-10 17:31:35 +0300

    ewqewqewq

--- controllers/ur-controller.js
+++ controllers/ur-controller.js
@@ -2,15 +2,13 @@ const OracleDB = require("oracledb");
 const UrService = require("../service/ur-service");
 const pool = require("../db/pool");
 
-
-
 class UrController {
-    getContrAgents = async (req, res) => {
-        const { search } = req.body;
-        try {
-          const connection = await OracleDB.getConnection(pool);
-          const result = await connection.execute(
-            `select * from ictdat.ur 
+  getContrAgents = async (req, res) => {
+    const { search } = req.body;
+    try {
+      const connection = await OracleDB.getConnection(pool);
+      const result = await connection.execute(
+        `select * from ictdat.ur 
             WHERE NDOV LIKE '${search}%'
             OR NDOV LIKE '$${search}'
             OR NDOV LIKE '${search}'
@@ -33,15 +31,69 @@ class UrController {
             OR NDOV LIKE '${search}_'
             OR NDOV LIKE '${search}_%'
             `
-          );
-          res.status(200).json(result.rows);
-          if (!result) {
-            res.status(401).json({ message: "error" });
-          }
-        } catch (error) {
-          console.log(error);
+      );
+      res.status(200).json(result.rows);
+      if (!result) {
+        res.status(401).json({ message: "error" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getContrAgentsPreRegister = async (req, res) => {
+    const { email, phone_number, company } = req.body;
+    console.log(req.body);
+
+    try {
+      const connection = await OracleDB.getConnection(pool);
+      const resultNameOrKodCheck = await connection.execute(
+        `SELECT * FROM ictdat.ur WHERE UPPER(ZKPO) = UPPER(:company) OR UPPER(NDOV) LIKE UPPER(:companyPattern)`,
+        {
+          company: company,
+          companyPattern: `%${company}%`,
         }
-      };
+      );
+      const resultPhoneCheck = await connection.execute(
+        `SELECT a.*,b.NKONTAKT as contact_name,b.PRIM as who,c.NDOV as COMPANY_NAME
+             FROM ictdat.KONTAKTVAL a
+             left join ictdat.kontakt b on a.KOD_KONTAKT = b.KOD
+             left join ictdat.ur c on b.KOD_UR = c.KOD
+             WHERE (VAL = :searchTerm OR VALIDX = :searchTerm 
+                   OR REPLACE(REPLACE(VALIDX, '(', ''), ')', '') = :searchTerm 
+                   OR REPLACE(REPLACE(VAL, '(', ''), ')', '') = :searchTerm)
+                OR LOWER(VAL) = LOWER(:searchTerm)
+                OR LOWER(VALIDX) = LOWER(:searchTerm)`,
+        {
+          searchTerm: phone_number,
+        }
+      );
+      const resultEmailCheck = await connection.execute(
+        `SELECT a.*,b.NKONTAKT as contact_name,b.PRIM as who,c.NDOV as COMPANY_NAME
+             FROM ictdat.KONTAKTVAL a
+             left join ictdat.kontakt b on a.KOD_KONTAKT = b.KOD
+             left join ictdat.ur c on b.KOD_UR = c.KOD
+             WHERE (VAL = :searchTerm OR VALIDX = :searchTerm 
+                   OR REPLACE(REPLACE(VALIDX, '(', ''), ')', '') = :searchTerm 
+                   OR REPLACE(REPLACE(VAL, '(', ''), ')', '') = :searchTerm)
+                OR LOWER(VAL) = LOWER(:searchTerm)
+                OR LOWER(VALIDX) = LOWER(:searchTerm)`,
+        {
+          searchTerm: email,
+        }
+      );
+      console.log("DASDASADS", resultNameOrKodCheck.rows);
+      console.log("DASDASADS", resultPhoneCheck.rows);
+      console.log("DASDASADS", resultEmailCheck.rows);
+
+      res.status(200).json({
+        name_kod: resultNameOrKodCheck.rows,
+        phone: resultPhoneCheck.rows,
+        email: resultEmailCheck.rows,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 module.exports = new UrController();
--- router/ur-routes.js
+++ router/ur-routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.post("/search", urController.getContrAgents);
 
+
+router.post("/search-pre-register", urController.getContrAgentsPreRegister);
+
 // authMiddleware,
 module.exports = router;
 // 6821
